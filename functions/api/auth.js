6c79abfe4f6d62cea490519775ec45568dfaa4bf
Jan <jvinch@pm.me>
2026-07-31 21:26:28 -0500

    feat: add Cloudflare functions for GitHub OAuth & configure Sveltia base_url

---
+++ functions/api/auth.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+  const { env } = context;
+  const client_id = env.GITHUB_CLIENT_ID;
+  const url = new URL(context.request.url);
+  
+  // Construct GitHub OAuth URL
+  const redirectUrl = new URL('https://github.com/login/oauth/authorize');
+  redirectUrl.searchParams.set('client_id', client_id);
+  redirectUrl.searchParams.set('redirect_uri', `${url.origin}/api/callback`);
+  redirectUrl.searchParams.set('scope', 'repo,user');
+  redirectUrl.searchParams.set('state', crypto.getRandomValues(new Uint8Array(12)).join(''));
+
+  return Response.redirect(redirectUrl.href, 301);
+}
